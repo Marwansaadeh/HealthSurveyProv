feff6f629c329e4efdf9ebc8d231e68ce848e31a
Marwan <marwansaad123@hotmail.com>
2021-10-06 19:08:01 +0200

    Add project files

---
+++ HealthSurveyProv/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealthSurveyProv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
